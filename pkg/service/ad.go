6797aa50891e95ee2d4b14c691902f9d245f0f43
peterxcli <peterxcli@gmail.com>
2024-03-02 18:51:33 +0800

    feat: Introducing AdService: Asynchronous Ad Management with Redis and GORM Integration
    - Add a new `AdService` in `service` package for ad management.
    - Implement `AdService` with methods to create and retrieve ads, and a subscription method, adhering to the `model.AdService` interface.
    - Introduce error handling with `ErrTimeout` and `ErrUnknown` for operation failures.
    - Incorporate external services such as `gorm` for database operations, `redis` for caching and message streaming, and `redislock` for distributed locking.
    - Implement a method `lockAndStoreAndPublish` to atomically lock, store an ad in the database, publish it to a Redis stream, and then release the lock, ensuring data consistency and integrity.
    - Utilize transactions to safely increment ad version numbers during database insertion.
    - Streamline ad creation process by generating a unique request ID, sending a creation request over a channel, and handling responses asynchronously, including timeout handling.
    - Provide a method to retrieve ads based on a request, supporting asynchronous communication and timeout handling.
    - Facilitate the creation of the `AdService` with a constructor function `NewAdService`, requiring dependencies such as a `runner.Runner`, `gorm.DB`, `redis.Client`, and `redislock.Client`.

---
+++ pkg/service/ad.go
@@ -0,0 +1,130 @@
+package service
+
+import (
+	"context"
+	"dcard-backend-2024/pkg/model"
+	"dcard-backend-2024/pkg/runner"
+	"fmt"
+	"time"
+
+	"github.com/bsm/redislock"
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
+	"gorm.io/gorm"
+)
+
+var (
+	ErrTimeout = fmt.Errorf("timeout")
+	ErrUnknown = fmt.Errorf("unknown error")
+)
+
+type AdService struct {
+	runner  *runner.Runner
+	db      *gorm.DB
+	redis   *redis.Client
+	locker  *redislock.Client
+	lockKey string
+}
+
+// Subscribe implements model.AdService.
+func (a *AdService) Subscribe(offset int) error {
+	panic("unimplemented")
+}
+
+// lockAndStoreAndPublish
+//
+// 1. locks the lockKey
+//
+// 2. stores the ad in the database, and set the version of the new ad to `SELECT MAX(version) FROM ad“ + 1
+//
+// 3. publishes the ad into redis stream. (ensure the message sequence number is the same as the ad's version)
+//
+// 4. releases the lock
+func (a *AdService) lockAndStoreAndPublish(ctx context.Context, ad *model.Ad) error {
+	lock, err := a.locker.Obtain(ctx, a.lockKey, 0, nil)
+	if err != nil {
+		return err
+	}
+	defer lock.Release(ctx)
+	txn := a.db.Begin()
+	if err := txn.Error; err != nil {
+		return err
+	}
+	var maxVersion int
+	if err := txn.Raw("SELECT MAX(version) FROM ad").Scan(&maxVersion).Error; err != nil {
+		txn.Rollback()
+		return err
+	}
+	ad.Version = maxVersion + 1
+	if err := txn.Create(ad).Error; err != nil {
+		txn.Rollback()
+		return err
+	}
+	err = txn.Commit().Error
+	if err != nil {
+		return err
+	}
+	_, err = a.redis.XAdd(ctx, &redis.XAddArgs{
+		Stream: "ad",
+		Values: ad,
+	}).Result()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// CreateAd implements model.AdService.
+func (a *AdService) CreateAd(ctx context.Context, ad *model.Ad) (string, error) {
+	err := a.lockAndStoreAndPublish(ctx, ad)
+	if err != nil {
+		return "", err
+	}
+
+	requestID := uuid.New().String()
+	a.runner.RequestChan <- &runner.CreateAdRequest{
+		Request: runner.Request{RequestID: requestID},
+		Ad:      ad,
+	}
+
+	select {
+	case resp := <-a.runner.ResponseChan[requestID]:
+		if resp, ok := resp.(*runner.CreateAdResponse); ok {
+			return resp.AdID, resp.Err
+		}
+	case <-time.After(3 * time.Second):
+		return "", ErrTimeout
+	}
+
+	return "", ErrUnknown
+}
+
+// GetAds implements model.AdService.
+func (a *AdService) GetAds(ctx context.Context, req *model.GetAdRequest) ([]*model.Ad, int, error) {
+	requestID := uuid.New().String()
+	a.runner.RequestChan <- &runner.GetAdRequest{
+		Request:      runner.Request{RequestID: requestID},
+		GetAdRequest: req,
+	}
+
+	select {
+	case resp := <-a.runner.ResponseChan[requestID]:
+		if resp, ok := resp.(*runner.GetAdResponse); ok {
+			return resp.Ads, resp.Total, resp.Err
+		}
+	case <-time.After(3 * time.Second):
+		return nil, 0, ErrTimeout
+	}
+
+	return nil, 0, ErrUnknown
+}
+
+func NewAdService(runner *runner.Runner, db *gorm.DB, redis *redis.Client, locker *redislock.Client) model.AdService {
+	return &AdService{
+		runner:  runner,
+		db:      db,
+		redis:   redis,
+		locker:  locker,
+		lockKey: "lock:ad",
+	}
+}
