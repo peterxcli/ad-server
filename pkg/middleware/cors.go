e45a69fab1e06c7a4190bc8d0bcd6d8063a3d690
peterxcli <peterxcli@gmail.com>
2024-03-07 01:15:25 +0800

    meow: allow all origin

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -4,8 +4,8 @@ import "github.com/gin-gonic/gin"
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		//TODO: add production frontend url in allow origin
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		origin := c.Request.Header.Get("Origin")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Content-Range, Accept-Ranges, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, grant_type, code")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
