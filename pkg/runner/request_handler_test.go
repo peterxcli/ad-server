99f2cc002363b9824bb857f33fd4a42c514b5783
peterxcli <peterxcli@gmail.com>
2024-03-08 15:58:53 +0800

    feat: Add handleCreateAdRequest test case to request_handler_test.go

--- pkg/runner/request_handler_test.go
+++ pkg/runner/request_handler_test.go
@@ -2,9 +2,14 @@ package runner
 
 import (
 	"context"
+	"dcard-backend-2024/pkg/inmem"
 	"dcard-backend-2024/pkg/model"
+	"reflect"
 	"testing"
 	"time"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRunner_IsRunning(t *testing.T) {
@@ -47,6 +52,71 @@ func TestRunner_IsRunning(t *testing.T) {
 		})
 	}
 }
+
+func TestRunner_handleCreateAdRequest(t *testing.T) {
+	type fields struct {
+		RequestChan  chan interface{}
+		ResponseChan map[string]chan interface{}
+		Store        model.InMemoryStore
+	}
+	type args struct {
+		req *CreateAdRequest
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name: "Test handleCreateAdRequest",
+			fields: fields{
+				RequestChan:  make(chan interface{}),
+				ResponseChan: make(map[string]chan interface{}),
+				Store:        inmem.NewInMemoryStore(),
+			},
+			args: args{
+				req: &CreateAdRequest{
+					Request: Request{RequestID: "test"},
+					Ad: &model.Ad{
+						ID:       uuid.New(),
+						Title:    "test",
+						Content:  "test",
+						StartAt:  model.CustomTime(time.Now().Add(-1 * time.Hour * 24)),
+						EndAt:    model.CustomTime(time.Now().Add(1 * time.Hour * 24)),
+						AgeStart: 18,
+						AgeEnd:   65,
+						Gender:   []string{"F", "M"},
+						Country:  []string{"TW"},
+						Platform: []string{"ios"},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &Runner{
+				RequestChan:  tt.fields.RequestChan,
+				ResponseChan: tt.fields.ResponseChan,
+				Store:        tt.fields.Store,
+			}
+			tt.fields.ResponseChan[tt.args.req.RequestID] = make(chan interface{})
+			go r.handleCreateAdRequest(tt.args.req)
+			select {
+			case resp := <-tt.fields.ResponseChan[tt.args.req.RequestID]:
+				if resp, ok := resp.(*CreateAdResponse); ok {
+					if resp.Err != nil {
+						t.Errorf("Runner.handleCreateAdRequest() = %v, want %v", resp.Err, nil)
+					}
+					assert.Equal(t, resp.AdID, tt.args.req.Ad.ID.String())
+				}
+			case <-time.After(3 * time.Second):
+				t.Errorf("Runner.handleCreateAdRequest() = %v, want %v", nil, nil)
+			}
+		})
+	}
+}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			r := &Runner{
