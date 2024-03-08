72a5f62ec87d16960ca9c1d52d1b9ce2cb01a409
peterxcli <peterxcli@gmail.com>
2024-03-08 15:29:59 +0800

    feat: Add test for Runner.IsRunning() method

--- pkg/runner/request_handler_test.go
+++ pkg/runner/request_handler_test.go
@@ -1,3 +1,43 @@
 package runner
 
-//TODO
+import (
+	"dcard-backend-2024/pkg/model"
+	"reflect"
+	"testing"
+)
+
+func TestRunner_IsRunning(t *testing.T) {
+	type fields struct {
+		RequestChan  chan interface{}
+		ResponseChan map[string]chan interface{}
+		Store        model.InMemoryStore
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name: "Test IsRunning",
+			fields: fields{
+				RequestChan:  make(chan interface{}),
+				ResponseChan: make(map[string]chan interface{}),
+				Store:        nil,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &Runner{
+				RequestChan:  tt.fields.RequestChan,
+				ResponseChan: tt.fields.ResponseChan,
+				Store:        tt.fields.Store,
+			}
+			go r.Start()
+			if got := r.IsRunning(); got != tt.want {
+				t.Errorf("Runner.IsRunning() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
