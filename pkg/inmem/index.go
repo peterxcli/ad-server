503ac47b556b100f97eccfeefdeb8802b216ab2b
peterxcli <peterxcli@gmail.com>
2024-03-12 23:12:17 +0800

    refactor: Refactor index structure with flexible tree structure

--- pkg/inmem/index.go
+++ pkg/inmem/index.go
@@ -2,183 +2,126 @@ package inmem
 
 import (
 	"dcard-backend-2024/pkg/model"
+	"fmt"
+	"log"
 
+	cmap "github.com/orcaman/concurrent-map/v2"
 	"github.com/wangjia184/sortedset"
 )
 
-// TODO: refactor: index https://chat.openai.com/share/09538e42-b82f-4e29-9d4f-e306ede70f04
-
-type QueryIndex struct {
-	// Ages maps a string representation of an age range to a CountryIndex
-	Ages map[uint8]*CountryIndex
+type IndexNode interface {
+	AddAd(ad *model.Ad)
+	GetAd(req *model.GetAdRequest) ([]*model.Ad, error)
+	DeleteAd(ad *model.Ad)
 }
 
-type CountryIndex struct {
-	// Countries maps country codes to PlatformIndex
-	Countries map[string]*PlatformIndex
+type FieldStringer struct {
+	Value interface{}
 }
 
-type PlatformIndex struct {
-	// Platforms maps platform names to GenderIndex
-	Platforms map[string]*GenderIndex
+func (f FieldStringer) String() string {
+	return fmt.Sprintf("%v", f.Value)
 }
 
-type GenderIndex struct {
-	// Genders maps gender identifiers to sets of Ad IDs
-	Genders map[string]*sortedset.SortedSet
-}
-
-type AdIndex interface {
-	// AddAd adds an ad to the index
-	AddAd(ad *model.Ad) error
-	// RemoveAd removes an ad from the index
-	RemoveAd(ad *model.Ad) error
-	// GetAdIDs returns the ad IDs that match the given query
-	GetAdIDs(req *model.GetAdRequest) ([]*model.Ad, error)
-}
-
-// AdIndexImpl thread-unsafe implementation of AdIndex
-type AdIndexImpl struct {
-	// index is the root index
-	index *QueryIndex
-}
-
-// AddAd implements AdIndex.
-func (a *AdIndexImpl) AddAd(ad *model.Ad) error {
-	targetCountries := append(ad.Country, "")
-	targetPlatforms := append(ad.Platform, "")
-	targetGenders := append(ad.Gender, "")
-	targetAges := []uint8{0}
-	for age := ad.AgeStart; age <= ad.AgeEnd; age++ {
-		targetAges = append(targetAges, age)
+func (g *IndexInternalNode) AddAd(ad *model.Ad) {
+	values, err := ad.GetValueByKey(g.Key)
+	if err != nil {
+		log.Printf("AddAd: Error getting value by key \"%s\": %s\n", g.Key, err)
+		return
 	}
-	for _, country := range targetCountries {
-		for _, platform := range targetPlatforms {
-			for _, gender := range targetGenders {
-				for _, age := range targetAges {
-					ageIndex, ok := a.index.Ages[age]
-					if !ok {
-						ageIndex = &CountryIndex{Countries: make(map[string]*PlatformIndex)}
-						a.index.Ages[age] = ageIndex
-					}
 
-					platformIndex, ok := ageIndex.Countries[country]
-					if !ok {
-						platformIndex = &PlatformIndex{Platforms: make(map[string]*GenderIndex)}
-						ageIndex.Countries[country] = platformIndex
-					}
-
-					genderIndex, ok := platformIndex.Platforms[platform]
-					if !ok {
-						genderIndex = &GenderIndex{Genders: make(map[string]*sortedset.SortedSet)}
-						platformIndex.Platforms[platform] = genderIndex
-					}
-
-					adSet, ok := genderIndex.Genders[gender]
-					if !ok {
-						adSet = sortedset.New()
-						genderIndex.Genders[gender] = adSet
-					}
-					adSet.AddOrUpdate(ad.ID.String(), sortedset.SCORE(ad.StartAt.T().Unix()), ad)
-				}
+	for _, v := range values {
+		field := FieldStringer{Value: v}
+		child, exists := g.Children.Get(field)
+		if !exists {
+			nextKey := model.Ad{}.GetNextIndexKey(g.Key) // This function should determine the next key based on the current key
+			if nextKey == "" {
+				// This is a leaf node
+				child = NewIndexLeafNode()
+			} else {
+				// This is an internal node
+				child = NewIndexInternalNode(nextKey)
 			}
+			g.Children.Set(field, child)
 		}
+
+		child.AddAd(ad)
 	}
-	return nil
 }
 
-// GetAdIDs implements AdIndex.
-func (a *AdIndexImpl) GetAdIDs(req *model.GetAdRequest) ([]*model.Ad, error) {
-	ageIndex, ok := a.index.Ages[req.Age]
-	if !ok {
-		return nil, ErrNoAdsFound
+// GetAd implements IndexNode.
+func (g *IndexInternalNode) GetAd(req *model.GetAdRequest) ([]*model.Ad, error) {
+	values, err := req.GetValueByKey(g.Key)
+	if err != nil {
+		return nil, fmt.Errorf("GetAd: Error getting value by key \"%s\": %s", g.Key, err)
 	}
 
-	platformIndex, ok := ageIndex.Countries[req.Country]
-	if !ok {
-		return nil, ErrNoAdsFound
+	Field := FieldStringer{Value: values}
+	child, exists := g.Children.Get(Field)
+	if !exists {
+		return nil, nil
 	}
 
-	genderIndex, ok := platformIndex.Platforms[req.Platform]
-	if !ok {
-		return nil, ErrNoAdsFound
-	}
-
-	adSet, ok := genderIndex.Genders[req.Gender]
-	if !ok {
-		return nil, ErrNoAdsFound
-	}
-
-	// get the ad IDs from the sorted set
-	result := adSet.GetByRankRange(req.Offset, req.Offset+req.Limit, false)
-
-	ads := make([]*model.Ad, 0, len(result))
-	for _, ad := range result {
-		ads = append(ads, ad.Value.(*model.Ad))
-	}
+	ads, err := child.GetAd(req)
 	return ads, nil
 }
 
-// RemoveAd implements AdIndex.
-func (a *AdIndexImpl) RemoveAd(ad *model.Ad) error {
-	targetCountries := append(ad.Country, "")
-	targetPlatforms := append(ad.Platform, "")
-	targetGenders := append(ad.Gender, "")
-	targetAges := []uint8{0}
-	for age := ad.AgeStart; age <= ad.AgeEnd; age++ {
-		targetAges = append(targetAges, age)
+// DeleteAd implements IndexNode.
+func (g *IndexInternalNode) DeleteAd(ad *model.Ad) {
+	values, err := ad.GetValueByKey(g.Key)
+	if err != nil {
+		log.Printf("Error getting value by key \"%s\": %s\n", g.Key, err)
+		return
 	}
-	for _, country := range targetCountries {
-		for _, platform := range targetPlatforms {
-			for _, gender := range targetGenders {
-				for _, age := range targetAges {
-					ageIndex, ok := a.index.Ages[age]
-					if !ok {
-						continue
-					}
 
-					platformIndex, ok := ageIndex.Countries[country]
-					if !ok {
-						continue
-					}
-
-					genderIndex, ok := platformIndex.Platforms[platform]
-					if !ok {
-						continue
-					}
-
-					adSet, ok := genderIndex.Genders[gender]
-					if !ok {
-						continue
-					}
-					adSet.Remove(ad.ID.String())
-
-					if adSet.GetCount() == 0 {
-						delete(genderIndex.Genders, gender)
-					}
-
-					if len(genderIndex.Genders) == 0 {
-						delete(platformIndex.Platforms, platform)
-					}
-
-					if len(platformIndex.Platforms) == 0 {
-						delete(ageIndex.Countries, country)
-					}
-
-					if len(ageIndex.Countries) == 0 {
-						delete(a.index.Ages, age)
-					}
-				}
-			}
+	for _, v := range values {
+		field := FieldStringer{Value: v}
+		child, exists := g.Children.Get(field)
+		if !exists {
+			continue
 		}
+
+		child.DeleteAd(ad)
 	}
-	return nil
 }
 
-func NewAdIndex() AdIndex {
-	return &AdIndexImpl{
-		index: &QueryIndex{
-			Ages: make(map[uint8]*CountryIndex),
-		},
+type IndexInternalNode struct {
+	Key      string                                       // The key this node indexes on, e.g., "country", "age"
+	Children cmap.ConcurrentMap[FieldStringer, IndexNode] // The children of this node
+}
+
+func NewIndexInternalNode(key string) IndexNode {
+	return &IndexInternalNode{
+		Key:      key,
+		Children: cmap.NewStringer[FieldStringer, IndexNode](),
+	}
+}
+
+type IndexLeafNode struct {
+	Ads *sortedset.SortedSet // map[string]*model.Ad
+}
+
+func (g *IndexLeafNode) AddAd(ad *model.Ad) {
+	g.Ads.AddOrUpdate(ad.ID.String(), sortedset.SCORE(ad.CreatedAt.T().Unix()), ad)
+}
+
+// GetAd implements IndexNode.
+func (g *IndexLeafNode) GetAd(req *model.GetAdRequest) ([]*model.Ad, error) {
+	ad := g.Ads.GetByRankRange(req.Offset, req.Offset+req.Limit, false)
+	ret := make([]*model.Ad, len(ad))
+	for i, a := range ad {
+		ret[i] = a.Value.(*model.Ad)
+	}
+	return ret, nil
+}
+
+// DeleteAd implements IndexNode.
+func (g *IndexLeafNode) DeleteAd(ad *model.Ad) {
+	g.Ads.Remove(ad.ID.String())
+}
+
+func NewIndexLeafNode() IndexNode {
+	return &IndexLeafNode{
+		Ads: sortedset.New(),
 	}
 }
